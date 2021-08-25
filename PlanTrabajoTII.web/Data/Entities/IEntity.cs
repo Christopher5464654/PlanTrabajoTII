fec074f898474736f628935984d7bde70bc1e030
jesus martinez carrera <martinez101jesus@outlook.com>
2021-08-25 17:26:06 -0500

    Interfaces y controladores

---
+++ PlanTrabajoTII.web/Data/Entities/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace PlanTrabajoTII.web.Data.Entities
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
--- PlanTrabajoTII.web/Startup.cs
+++ PlanTrabajoTII.web/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -37,6 +38,11 @@ namespace PlanTrabajoTII.web
                 cfg.Password.RequiredUniqueChars = 0;
             }).AddEntityFrameworkStores<DataContext>();
             services.AddControllersWithViews();
+
+            services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
