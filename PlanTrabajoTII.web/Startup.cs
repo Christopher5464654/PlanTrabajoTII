ccc0552f38533cad4ad32a97830950413c6b5ca8
RecklessTaco <bautgus@gmail.com>
2021-08-26 22:19:08 -0500

    Cambios Clase Fong 25/Ago. Se instalaron paquetes para poder hacer migraciones y bases de datos, junto con la creación de interfaces de Entities y GenericRepository

--- PlanTrabajoTII.web/Startup.cs
+++ PlanTrabajoTII.web/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -36,6 +37,11 @@ namespace PlanTrabajoTII.web
                 cfg.Password.RequiredLength = 0;
                 cfg.Password.RequiredUniqueChars = 0;
             }).AddEntityFrameworkStores<DataContext>();
+
+            services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
             services.AddControllersWithViews();
         }
 
